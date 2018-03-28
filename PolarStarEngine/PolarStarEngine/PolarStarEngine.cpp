dd110b9ce5f71d067e79ab734a07d191a7ddeb5c
AaronMacaulay <vidjunkie@yahoo.ca>
2018-03-28 00:30:40 -0400

    Physics Work

---
+++ PolarStarEngine/PolarStarEngine/PolarStarEngine.cpp
@@ -0,0 +1,228 @@
+// #ifndef
+
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <assert.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#include <Windows.h>
+#include <WinBase.h>
+#include <direct.h>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+#include "PolarStarEngine.h"
+
+#include "GameObject.h"
+
+//game play gameobjects
+#include "Bill.h"
+#include "Rigidbody.h"
+#include "SpriteRenderer.h"
+
+sf::RenderWindow* PolarStarEngine::render_window;
+
+PolarStarEngine::PolarStarEngine() : is_debug_mode(true), scene_root(), physics_engine() {
+	std::cout << "starting app \n";
+	engine_current_state = Uninitialized;
+	std::thread hardware_check_thread(&PolarStarEngine::check_hardware, this);
+
+	this->render_window = new sf::RenderWindow();
+	this->init_graphics();
+	this->display_splash_screen();
+
+	hardware_check_thread.join();
+
+	//this->main_loop();
+}
+
+void PolarStarEngine::check_hardware() {
+	std::cout << "Starting init" << std::endl;
+
+	this->check_enough_disk_space();
+	this->get_cpu_speed();
+	this->get_cpu_architecture();
+	this->check_memory();
+	this->check_joypads();
+
+	engine_current_state = Initialized;
+
+	std::cout << "hardware checking . . . \n" << std::endl;
+
+	return;
+}
+
+
+unsigned int PolarStarEngine::check_enough_disk_space() {
+	const DWORDLONG disk_bytes_needed = 300000000;
+
+	int const drive = _getdrive();
+	//int const drive = 0;
+	struct _diskfree_t diskfree;
+	_getdiskfree(drive, &diskfree);
+	unsigned __int64 const needed_clusters = disk_bytes_needed /
+		(diskfree.sectors_per_cluster*diskfree.bytes_per_sector);
+	if (diskfree.avail_clusters < needed_clusters) {
+		std::cout << "ERROR NOT ENOUGH SPACE" << std::endl;
+		assert(false);
+	}
+	return true;
+}
+
+unsigned int PolarStarEngine::get_cpu_speed() {
+
+	DWORD buffer_size = sizeof(DWORD);
+	DWORD mhz = 0;
+	DWORD value_type = REG_DWORD;
+	HKEY hive_key;
+
+	long error_type = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+		"HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0, KEY_READ, &hive_key);
+	if (error_type == ERROR_SUCCESS) {
+		RegQueryValueEx(hive_key,
+			"~MHz", NULL, &value_type, (LPBYTE)&mhz, &buffer_size);
+	}
+	else {
+		std::cout << "Could get CPU speed";
+		return 0;
+	}
+
+	assert(mhz > 1000);
+
+	return mhz;
+}
+
+std::string PolarStarEngine::get_cpu_architecture() {
+
+	TCHAR architecture[1024];
+	DWORD architecture_length = sizeof(architecture);
+	DWORD value_type = REG_SZ;
+	HKEY hive_key;
+	long error_type = RegOpenKeyEx(HKEY_LOCAL_MACHINE,
+		TEXT("HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0"), 0, KEY_READ, &hive_key);
+	if (error_type == ERROR_SUCCESS) {
+		RegQueryValueEx(hive_key, TEXT("ProcessorNameString"), NULL, &value_type,
+			reinterpret_cast<LPBYTE>(&architecture), &architecture_length);
+	}
+	std::cout << architecture << std::endl;
+
+	return "CPU WORKS";
+}
+
+void PolarStarEngine::check_memory() {
+
+	MEMORYSTATUSEX memoryStatus = { sizeof memoryStatus };
+	GlobalMemoryStatusEx(&memoryStatus);
+
+	const DWORDLONG phyiscal_memory = memoryStatus.ullAvailPhys;
+
+	DWORDLONG virtual_memory = memoryStatus.ullAvailVirtual;
+
+	assert(phyiscal_memory > 90555520);
+	assert(virtual_memory > 90555520);
+
+	std::cout << "You have " << phyiscal_memory << " available bytes for physical memory." << std::endl;
+
+	std::cout << "You have " << virtual_memory << " available bytes for virtual memory." << std::endl;
+
+}
+
+void PolarStarEngine::check_joypads() {
+	int i = 0;
+	while (sf::Joystick::isConnected(i)) {
+		std::cout << "Joystick " << i << " is connected" << std::endl;
+		i++;
+	}
+}
+
+void PolarStarEngine::init_graphics() {
+	this->render_window->create({ 1024, 709 }, "PolarStarEngine");
+}
+
+void PolarStarEngine::display_splash_screen() {
+
+	sf::Texture image;
+
+	if (!image.loadFromFile("..\\Assets\\polarstar.jpg"))
+	{
+		std::cout << "Error could not load splash screen image" << std::endl;
+		return;
+	}
+
+	sf::Sprite sprite(image);
+
+	while (engine_current_state != Initialized)
+	{
+		render_window->clear();
+		render_window->draw(sprite);
+		this->render_window->display();
+	}
+}
+
+void PolarStarEngine::setup_scene() {
+
+	Bill* bill = new Bill();
+
+	GameObject* somaone = new GameObject("Soma", nullptr);
+	SpriteRenderer* sprite_renderer = new SpriteRenderer();
+	sprite_renderer->set_sprite("..\\Assets\\Soma.png");
+	somaone->add_component(sprite_renderer);
+
+	BoxCollider *box_collider = new BoxCollider(sprite_renderer->get_sprite().getGlobalBounds().width * 0.5f,
+		sprite_renderer->get_sprite().getGlobalBounds().height * 0.5f);
+	somaone->add_component(box_collider);
+
+	Rigidbody* rigidbody = new Rigidbody();
+	rigidbody->mass = 0.0f;
+	rigidbody->bounciness = 0.6f;
+	somaone->add_component(rigidbody);
+	somaone->transform->setPosition(300.0f, 100.0f);
+
+	GameObject* somatwo = new GameObject("Soma2", nullptr);
+	SpriteRenderer* sprite_renderer2 = new SpriteRenderer();
+	sprite_renderer2->set_sprite("..\\Assets\\Soma.png");
+	somatwo->add_component(sprite_renderer2);
+
+	BoxCollider *box_collider2 = new BoxCollider(sprite_renderer2->get_sprite().getGlobalBounds().width * 0.5f,
+		sprite_renderer2->get_sprite().getGlobalBounds().height * 0.5f);
+	somatwo->add_component(box_collider2);
+
+	Rigidbody* rigidbody2 = new Rigidbody();
+	rigidbody2->mass = 5.0f;
+	rigidbody2->bounciness = 0.6f;
+	//rigidbody2->gravity.x = 0.0001f;
+	//rigidbody2->gravity.y = 0.0f;
+	somatwo->add_component(rigidbody2);
+	somatwo->transform->setPosition(600.0f, 100.0f);
+
+}
+
+void PolarStarEngine::main_loop() {
+
+	this->setup_scene();
+	//this->scene_root.start();
+
+	sf::Clock delta_time_clock;
+
+	while (this->render_window->isOpen())
+	{
+
+		sf::Event event;
+
+		while (this->render_window->pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				this->render_window->close();
+		}
+
+		float delta_time = (float)delta_time_clock.restart().asMilliseconds();
+		this->render_window->clear();
+		this->physics_engine.updates_physics(delta_time);
+		this->scene_root.update(delta_time);
+		this->render_window->display();
+	}
+}
---
+++ PolarStarEngine/PolarStarEngine/main.cpp
@@ -0,0 +1,28 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "PolarStarEngine.h"
+
+int main()
+{
+	PolarStarEngine application;
+	application.main_loop();
+
+	system("pause");
+
+	sf::RenderWindow window({ 1024,709 }, "Hello there");
+	window.setFramerateLimit(30);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.display();
+	}
+}
